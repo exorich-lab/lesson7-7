c95ac496cf16ddd0732f0f8163c9ab51b92b6a0f
sergejapetenok <exorich@yandex.ru>
2020-02-28 21:13:29 +0400

    Security Bug Fixed

--- generate_token.rb
+++ generate_token.rb
@@ -1,7 +1,5 @@
-#
-# Решил погуглив
-# Reference
-# https://gist.github.com/mbajur/2aba832a6df3fc31fe7a82d3109cb626
-# https://stackoverflow.com/questions/22656013/how-to-randomise-upper-lower-case-of-a-string-in-ruby
-#
-puts rand(36**8).to_s(36).chars.map { |c| (rand 2) == 0 ? c : c.upcase }.join
+# Наполняем массив всеми символами
+generate = [('a'..'z'), (0..9), ('A'..'Z')].map(&:to_a).flatten
+#  Генерим токен на 8 символов в блоке при помощи rand из массива generate
+token = (0...8).map { generate[rand(generate.length)] }.join
+puts token
